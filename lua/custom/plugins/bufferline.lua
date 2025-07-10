af217f96d1cbcafe68f0083936dda64ef35635f1
JPHarding <gamerz1497@live.com>
2025-07-10 17:15:38 +0200

    bufferline next, prev & locked-in mason plugins.

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -7,5 +7,7 @@ return {
     require('bufferline').setup {}
     vim.keymap.set('n', '<leader>.', '<cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right' })
     vim.keymap.set('n', '<leader>,', '<cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left' })
+    vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer (BufferLine order)' })
+    vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer (BufferLine order)' })
   end,
 }
