2973842f1c83ae9a7d06fc4f543c6ae4c0fb35a9
JPHarding <gamerz1497@live.com>
2025-07-10 22:28:35 +0200

    setup more lsp and linters

--- init.lua
+++ init.lua
@@ -685,6 +685,11 @@ require('lazy').setup({
         ruff = {},
         csharp_ls = {},
         debugpy = {},
+        intelephense = {},
+        html = {},
+        cssls = {},
+        htmlhint = {},
+        stylelint = {},
         --
 
         lua_ls = {
@@ -772,10 +777,19 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
+        python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+        html = { 'htmlhint' },
+        css = { 'stylelint' },
+
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        htmlhint = {
+          command = 'htmlhint',
+          args = { '--stdin' },
+          stdin = true,
+        },
       },
     },
   },
