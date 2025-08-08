cf6ba36d14fb94f0070341ad2a8094ca2c934ca7
JPHarding <gamerz1497@live.com>
2025-08-08 14:37:56 +0200

    New bufferline commands
    ,, go back
    .. go forwards in bufferline.

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -14,7 +14,7 @@ return {
     }
     vim.keymap.set('n', '<leader>.', '<cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right' })
     vim.keymap.set('n', '<leader>,', '<cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left' })
-    vim.keymap.set('n', '[b', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer (BufferLine order)' })
-    vim.keymap.set('n', ']b', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer (BufferLine order)' })
+    vim.keymap.set('n', ',,', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Previous buffer (BufferLine order)' })
+    vim.keymap.set('n', '..', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next buffer (BufferLine order)' })
   end,
 }
