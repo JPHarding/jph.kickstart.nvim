efa3574866173e543e840d287af3ef9eb23396a0
JPHarding <gamerz1497@live.com>
2026-02-25 17:14:42 +0200

    added mason dependency
    removed custom plugins init.lua file as it is not doing anything.

--- init.lua
+++ init.lua
@@ -525,6 +525,7 @@ require('lazy').setup({
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
+      { 'mason-org/mason-lspconfig.nvim', opts = {} },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
@@ -712,7 +713,7 @@ require('lazy').setup({
         -- debuggers
         debugpy = {},
 
-        ['lua-language-server'] = {
+        lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
           -- capabilities = {},
@@ -737,7 +738,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua-language-server', -- Lua Language server
+        'lua_ls', -- Lua Language server
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
@@ -751,7 +752,7 @@ require('lazy').setup({
       end
 
       -- Special Lua Config, as recommended by neovim help docs
-      vim.lsp.config('lua-language-server', {
+      vim.lsp.config('lua_ls', {
         on_init = function(client)
           if client.workspace_folders then
             local path = client.workspace_folders[1].name
